a2af3cff4a3a7ed0227f104be12b901d583485ac
Asif Al Mahmud <asifalmahmud1990@gmail.com>
2023-04-16 21:17:35 +0600

    add email in userSchema, create getUser and updateUser handlers in usersController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -8,51 +8,103 @@ const bcrypt = require("bcrypt");
 // @desc Get a user / User login
 // @route GET /users
 // @access Private
-const getUser = asyncHandler(async (req, res) => {});
+const getUser = asyncHandler(async (req, res) => {
+    const { id } = req.body;
+
+    const user = await User.findById(id).lean().exec();
+    if (!user) {
+        return res
+            .status(400)
+            .json({ status: "fail", message: "User not found" });
+    }
+    res.status(200).json({ status: "success", data: user });
+});
 
 // @desc Create new user / User signup
 // @route POST /users
 // @access Private
 const createNewUser = asyncHandler(async (req, res) => {
-  const { username, password } = req.body;
+    const { username, email, password } = req.body;
 
-  // Confirm data
-  if (!username || !password) {
-    return res
-      .status(400)
-      .json({ status: "fail", message: "All fields are required" });
-  }
+    // Confirm data
+    if (!username || !email || !password) {
+        return res
+            .status(400)
+            .json({ status: "fail", message: "All fields are required" });
+    }
 
-  // Check for duplicate
-  const duplicate = await User.findOne({ username }).lean().exec();
-  if (duplicate) {
-    return res
-      .status(409)
-      .json({ status: "fail", message: "Duplicate username" });
-  }
+    // Check for duplicate
+    const duplicate = await User.findOne({ email }).lean().exec();
+    if (duplicate) {
+        return res
+            .status(409)
+            .json({ status: "fail", message: "Duplicate email" });
+    }
 
-  // Hash the recieved password
-  const hashedPwd = await bcrypt.hash(password, 10); // 10 salt rounds
+    // Hash the recieved password
+    const hashedPwd = await bcrypt.hash(password, 10); // 10 salt rounds
 
-  const userObject = { username, password: hashedPwd };
+    const userObject = { username, password: hashedPwd };
 
-  // Create and store new user
-  const user = await User.create(userObject);
+    // Create and store new user
+    const user = await User.create(userObject);
 
-  if (user) {
-    res.status(201).json({
-      status: "success",
-      message: `New user ${user.username} created`,
-    });
-  } else {
-    res.status(400).json({ message: "Invalid user data received" });
-  }
+    if (user) {
+        res.status(201).json({
+            status: "success",
+            message: `New user ${user.username} created`,
+        });
+    } else {
+        res.status(400).json({ message: "Invalid user data received" });
+    }
 });
 
 // @desc Update a user
 // @route PATCH /users
 // @access Private
-const updateUser = asyncHandler(async (req, res) => {});
+const updateUser = asyncHandler(async (req, res) => {
+    const { id, username, email, password } = req.body;
+
+    let updateData = req.body;
+    delete updateData.id;
+
+    const user = await User.findById(id).exec();
+
+    if (!user) {
+        return res
+            .status(400)
+            .json({ status: "fail", message: "User not found" });
+    }
+
+    if (email) {
+        // Check for duplicate email
+        const duplicate = await User.findOne({ email }).lean().exec();
+        if (duplicate && duplicate?._id.toString() !== id) {
+            return res
+                .status(409)
+                .json({ status: "fail", message: "Email already in use" });
+        }
+    }
+
+    if (password) {
+        // Hash password
+        updateData.password = await bcrypt.hash(password, 10);
+    }
+
+    const updatedUser = await User.findOneAndUpdate(
+        { _id: id },
+        { $set: updateData }
+    );
+
+    if (updatedUser) {
+        res.status(200).json({ status: "success", data: updatedUser });
+    } else {
+        res.status(400).json({
+            status: "fail",
+            message: "Something went wrong",
+        });
+    }
+});
 
 // @desc Delete a user
 // @route DELETE /users
@@ -60,8 +112,8 @@ const updateUser = asyncHandler(async (req, res) => {});
 const deleteUser = asyncHandler(async (req, res) => {});
 
 module.exports = {
-  getUser,
-  createNewUser,
-  updateUser,
-  deleteUser,
+    getUser,
+    createNewUser,
+    updateUser,
+    deleteUser,
 };
--- server.js
+++ server.js
@@ -16,10 +16,10 @@ const PORT = process.env.PORT || 4000;
 connectDB();
 
 if (process.env.NODE_ENV === "development") {
-  const morgan = require("morgan");
-  app.use(morgan("dev"));
+    const morgan = require("morgan");
+    app.use(morgan("dev"));
 } else {
-  console.log(process.env.NODE_ENV);
+    console.log(process.env.NODE_ENV);
 }
 
 app.use(logger);
@@ -37,29 +37,29 @@ app.use("/", require("./routes/root"));
 app.use("/api/v1/users", require("./routes/userRoutes"));
 
 app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({
-      message: "404 Not Found",
-    });
-  } else {
-    res.type("txt").send("404 not found");
-  }
+    res.status(404);
+    if (req.accepts("html")) {
+        res.sendFile(path.join(__dirname, "views", "404.html"));
+    } else if (req.accepts("json")) {
+        res.json({
+            message: "404 Not Found",
+        });
+    } else {
+        res.type("txt").send("404 not found");
+    }
 });
 
 app.use(errorHandler);
 
 mongoose.connection.once("open", () => {
-  console.log("Connected to MongoDB");
-  app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
+    console.log("Connected to MongoDB");
+    app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`));
 });
 
 mongoose.connection.on("error", (err) => {
-  console.log(err);
-  logEvents(
-    `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
-    "mongoErrLog.log"
-  );
+    console.log(err);
+    logEvents(
+        `${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`,
+        "mongoErrLog.log"
+    );
 });
